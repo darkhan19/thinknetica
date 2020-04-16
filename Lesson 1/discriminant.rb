0c945893de7363cd7287da75b405e48b272ce7d7
Darkhan Zhumabekov <darkhan.zhumabekov@gmail.com>
2020-04-16 19:22:42 +0600

    Lesson 1 correted

--- Lesson 1/discriminant.rb
+++ Lesson 1/discriminant.rb
@@ -4,18 +4,17 @@ puts "Введите коефициент b"
 b = gets.chomp.to_i
 puts "Введите коефициент c"
 c = gets.chomp.to_i
-d=(b**2)-(4*a*c)
-b = -1*b
+d = (b**2) - (4 * a * c)
+b = -1 * b
 
 if d < 0 
   puts "Корней нет"
 elsif d > 0  
   sqrtd =  Math.sqrt(d) 
-  x1 = (b+sqrtd)/(2*a)
-  x2 = (b-sqrtd)/(2*a)
+  x1 = (b + sqrtd) / (2 * a)
+  x2 = (b - sqrtd) / (2 * a)
   print "2 корня, x1 = #{x1}, x2 = #{x2}"
-else d == 0 
-  sqrtd =  Math.sqrt(d) 
-  x1 = (b+sqrtd)/(2*a)
+else #d == 0 
+  x1 = b / (2 * a)
   print "1 корень = #{x1}"
 end
